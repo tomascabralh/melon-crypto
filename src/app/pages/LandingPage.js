b039bd69faf2d0723ca9d4ec107689508b45f458
Tomas Cabral Hunter <tomas.cabral@fierastudio.com>
2021-11-25 18:06:20 -0300

    add chakra ui

---
+++ src/app/pages/LandingPage.js
@@ -0,0 +1,15 @@
+import { Center, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <Center padding={4}>
+      <VStack>
+        <Text fontSize={48}>🍈 Melon Crypto 🍈</Text>
+        <Text fontSize={40}>🍈 The best crypto news around 🍈</Text>
+      </VStack>
+    </Center>
+  );
+};
+
+export default LandingPage;
