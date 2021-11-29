902b8d07fd1281b0be94e468052bdb8729298a5a
unknown <juanchifassio@gmail.com>
2021-11-28 22:23:39 -0300

    navbar

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,37 +1,48 @@
 import { Input } from "@chakra-ui/input";
-import { Box, Flex, Text, Link} from "@chakra-ui/layout";
+import { Box, Flex, Text, Link } from "@chakra-ui/layout";
 import React from "react";
 
 function Header() {
   return (
-    <Box>
-      <Flex
+    <Box h="54" pos="fixed" w="100%">
+      <Box
         py={{ base: 2 }}
         px={{ base: 4 }}
         bgColor="#9AE6B4"
         borderBottom="2px"
         borderColor="#48BB78"
         borderBottomStartRadius="md"
+        maxH="58"
+        justify="center"
+        whiteSpace="nowrap"
       >
-        <Flex  me={4} mt={2}>
-        <Box me={10}>
-        <Link href='/'><Text>🍈 Melon Crypto</Text></Link>
-        </Box>
-        <Box me={6}>
-          <Link><Text>Cryptos</Text></Link>
-        </Box>
-        <Box me={6}>
-          <Link><Text>NFTs</Text></Link>
-        </Box>
-        <Box me={700}>
-          <Link><Text>News</Text></Link>
-        </Box>
-        <Box right  me={6}>
-          <Input size="xs" variant="flushed" placeholder="Search . . ."/>
-        </Box>
+        <Flex me={4} mt={2} mb={2}>
+          <Box w="200px" me={8}>
+            <Link href="/">
+              <Text>🍈 Melon Crypto</Text>
+            </Link>
+          </Box>
+          <Box me={6}>
+            <Link>
+              <Text textAlign="center">Cryptos</Text>
+            </Link>
+          </Box>
+          <Box me={6}>
+            <Link>
+              <Text>NFTs</Text>
+            </Link>
+          </Box>
+          <Box me={6}>
+            <Link>
+              <Text>News</Text>
+            </Link>
+          </Box>
+          <Box w="100%"></Box>
+          <Box right me={10} w="500px">
+            <Input size="sm" variant="flushed" placeholder="Search . . ." />
+          </Box>
         </Flex>
-
-      </Flex>
+      </Box>
     </Box>
   );
 }
