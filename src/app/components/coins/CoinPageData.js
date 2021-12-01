1ff43dada4148dec8cd021503df752f0f4db8115
Tomas Cabral Hunter <tomas.cabral@fierastudio.com>
2021-12-01 20:28:32 -0300

    fix

--- src/app/components/coins/CoinPageData.js
+++ src/app/components/coins/CoinPageData.js
@@ -5,36 +5,46 @@ import axios from "axios";
 
 const CoinPageData = () => {
   const { id } = useParams();
-  const [coin, setCoin] = useState([]);
+  const [coin, setCoin] = useState();
 
   useEffect(() => {
     axios.get(`https://api.coingecko.com/api/v3/coins/${id}`).then((res) => {
       setCoin(res.data);
     });
-  }, [id, coin]);
-
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
-      <Box w="100%">
-        <Flex p="20px" m="50px">
-          <Box minW="200px" mr="100px">
-            <img src={coin.image?.large} alt={coin.name} />
+      {coin && (
+        <>
+          <Box w="100%">
+            <Flex p="20px" m="50px">
+              <Box minW="200px" mr="100px">
+                <img src={coin.image?.large} alt={coin.name} />
+              </Box>
+              <Box maxH="200px" overflow="auto">
+                {coin.description?.en}
+              </Box>
+            </Flex>
           </Box>
-          <Box maxH="200px" overflow="auto">
-            {coin.description?.en}
+          <Box w="100%" h={550}>
+            <Flex px="20px" mx="50px">
+              <Box
+                minW="200px"
+                maxW="250px"
+                maxH={550}
+                mr="50px"
+                overflow="auto"
+              >
+                {coin.description?.en}
+              </Box>
+              <Box maxH={550} overflow="auto">
+                {coin.description?.en} {coin.description?.en}
+              </Box>
+            </Flex>
           </Box>
-        </Flex>
-      </Box>
-      <Box w="100%" h={550}>
-        <Flex px="20px" mx="50px">
-          <Box minW="200px" maxW="250px" maxH={550} mr="50px" overflow="auto">
-            {coin.description?.en}
-          </Box>
-          <Box maxH={550} overflow="auto">
-            {coin.description?.en} {coin.description?.en}
-          </Box>
-        </Flex>
-      </Box>
+        </>
+      )}
     </>
   );
 };
