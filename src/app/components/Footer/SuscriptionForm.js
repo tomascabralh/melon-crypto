38a20ec835adbd5c0d67689c628abd1ea3c34526
unknown <juanchifassio@gmail.com>
2021-12-19 16:04:55 -0300

    newsletter popup

--- src/app/components/Footer/SuscriptionForm.js
+++ src/app/components/Footer/SuscriptionForm.js
@@ -2,11 +2,25 @@ import React from "react";
 import { Box, Text, Stack } from "@chakra-ui/layout";
 import { Input } from "@chakra-ui/input";
 import { Button } from "@chakra-ui/button";
+import { useToast } from "@chakra-ui/react";
 import { useColorModeValue } from "@chakra-ui/color-mode";
 
 const Suscription = () => {
   const bg = useColorModeValue("white", "gray.900");
   const color = useColorModeValue("black", "gray.50");
+  const toast = useToast();
+
+  function Popup(e) {
+    e.preventDefault();
+    toast({
+      title: "Thanks for joining our newsletter!",
+      description: "This is just a success popup xd, u wont receive any emails",
+      status: "success",
+      duration: 9000,
+      isClosable: true,
+    });
+    e.target.reset();
+  }
 
   return (
     <Box
@@ -28,21 +42,30 @@ const Suscription = () => {
         so you don't miss a single newsletter.
       </Text>
       <Stack>
-        <Input
-          size="sm"
-          background={bg}
-          color={color}
-          rounded="md"
-          px={2}
-          variant="flushed"
-          placeholder="Enter your email"
-          type="email"
-          maxWidth={360}
-          isReadOnly
-        />
-        <Button colorScheme="blue" variant="solid" maxWidth={120}>
-          SEND
-        </Button>
+        <form onSubmit={Popup}>
+          <Stack>
+            <Input
+              size="sm"
+              background={bg}
+              color={color}
+              rounded="md"
+              px={2}
+              variant="flushed"
+              placeholder="Enter your email"
+              type="email"
+              maxWidth={360}
+              isRequired
+            />
+            <Button
+              colorScheme="blue"
+              variant="solid"
+              maxWidth={120}
+              type="submit"
+            >
+              SEND
+            </Button>
+          </Stack>
+        </form>
       </Stack>
     </Box>
   );
